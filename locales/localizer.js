7d3a559e3e35112756b943d947ad05b5da95c682
Bryan Mailloux <Bryan.Mailloux@D2L.com>
2020-07-31 15:32:17 -0400

    US117429 Default to english translations (always load en.js)

--- locales/localizer.js
+++ locales/localizer.js
@@ -3,9 +3,18 @@ import { LocalizeMixin } from '@brightspace-ui/core/mixins/localize-mixin';
 
 export const Localizer = superclass => class extends LocalizeMixin(superclass) {
 
+	/**
+	 * @param {string[]} langs - contains locales and languages in order of preference
+	 * e.g. ['fr-fr', 'fr', 'en-us', 'en']
+	 */
 	static async getLocalizeResources(langs) {
 		let translations;
+		// always load in English translations, so we have something to default to
+		const enTranslations = await import('./en.js');
+
 		try {
+			// not sure why for await..of syntax is required here.
+			// In debugger langs looks like a regular array, but maybe it's really an async iterable under the hood
 			for await (const lang of langs) {
 				switch (lang) {
 					case 'ar':
@@ -54,16 +63,21 @@ export const Localizer = superclass => class extends LocalizeMixin(superclass) {
 				if (translations && translations.default) {
 					return {
 						language: lang,
-						resources: translations.default
+						resources: Object.assign(enTranslations.default, translations.default)
 					};
 				}
 			}
-		} catch (err) {
-			// can happen if the langterms file for a language doesn't exist
-			translations = await import('./en.js');
+
 			return {
 				language: 'en',
-				resources: translations.default
+				resources: enTranslations.default
+			};
+
+		} catch (err) {
+			// can happen if the langterms file for a language doesn't exist
+			return {
+				language: 'en',
+				resources: enTranslations.default
 			};
 		}
 	}
