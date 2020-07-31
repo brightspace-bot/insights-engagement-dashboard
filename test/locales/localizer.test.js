76435f15dff305f7b00054eb8622892a7635bdac
Bryan Mailloux <Bryan.Mailloux@D2L.com>
2020-07-31 15:55:55 -0400

    US117429 Localizer test

---
+++ test/locales/localizer.test.js
@@ -0,0 +1,15 @@
+import {expect} from '@open-wc/testing';
+import {Localizer} from '../../locales/localizer';
+
+class MockLocalizedObjectSuperclass {}
+class MockLocalizedObject extends Localizer(MockLocalizedObjectSuperclass) {}
+
+describe('Localizer', () => {
+	it('should default to English if the lang file is not found', async() => {
+		const langTerms = await MockLocalizedObject.getLocalizeResources(['not a supported language']);
+
+		expect(langTerms.language).to.equal('en');
+	});
+
+	// whenever we get actual langterms files: add a test that makes sure the correct langterms are loaded in
+});
