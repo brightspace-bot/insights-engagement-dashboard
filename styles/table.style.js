bdf76e10ed5f10f3902f791237708baa928b20ab
Bryan Mailloux <Bryan.Mailloux@D2L.com>
2020-08-11 11:02:10 -0400

    US119132 Table style edits

--- styles/table.style.js
+++ styles/table.style.js
@@ -8,12 +8,13 @@ export const tableStyle = css`
 
 .d2l-table {
 	background-color: #fff;
-	border: 1px solid var(--d2l-color-mica);
-	border-radius: 8px 8px 0 0;
+	border-collapse: separate;
+	border-spacing: 0;
 	width: 100%;
 	text-align: left;
 	font-weight: normal;
 }
+
 .d2l-table-header {
 	color: var(--d2l-color-ferrite);
 	background-color: var(--d2l-color-regolith);
@@ -27,6 +28,34 @@ export const tableStyle = css`
 	vertical-align: middle;
 	padding: 10px 20px;
 	height: 41px; /* min-height to be 62px including border */
+	border-right: 1px solid var(--d2l-color-mica);
+	border-bottom: 1px solid var(--d2l-color-mica);
+}
+
+/* Table cell border and radius */
+/* to get a radius on all corners *and* exactly 1px inner borders */
+table tr:first-child th {
+	border-top: 1px solid var(--d2l-color-mica);
+}
+
+table tr th:first-child,td:first-child {
+	border-left: 1px solid var(--d2l-color-mica);
+}
+
+table tr:first-child th:first-child {
+	border-top-left-radius: 8px;
+}
+
+table tr:first-child th:last-child {
+	border-top-right-radius: 8px;
+}
+
+table tr:last-child td:first-child {
+	border-bottom-left-radius: 8px;
+}
+
+table tr:last-child td:last-child {
+	border-bottom-right-radius: 8px;
 }
 
 th {
