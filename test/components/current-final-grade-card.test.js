b1a6647a0fc6f0dae40562364fd970152a2464df
Bryan Mailloux <Bryan.Mailloux@D2L.com>
2020-09-03 09:47:07 -0400

    US118248 Experimental fix for flaky tests

--- test/components/current-final-grade-card.test.js
+++ test/components/current-final-grade-card.test.js
@@ -15,7 +15,9 @@ describe('d2l-insights-current-final-grade-card', () => {
 	});
 
 	describe('accessibility', () => {
-		it('should pass all axe tests', async() => {
+		it('should pass all axe tests', async function() {
+			this.timeout(3000);
+
 			const el = await fixture(html`<d2l-insights-current-final-grade-card .data="${data}"></d2l-insights-current-final-grade-card>`);
 			await expect(el).to.be.accessible();
 		});
