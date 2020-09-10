df77811feb35cbd12c4a8b20579d3924fe37fb05
Vitalii Misechko <Vitalii.Misechko@d2l.com>
2020-09-10 20:17:20 +0300

    Add localization

--- locales/en.js
+++ locales/en.js
@@ -45,5 +45,7 @@ export default {
 	"components.insights-current-final-grade-card.numberOfStudents": "Number of Students",
 	"components.insights-current-final-grade-card.textLabel": "This chart displays the current final grade for each user per course",
 
-	"components.insights-engagement-dashboard.overdueAssignments": "Users currently have one or more overdue assignments."
+	"components.insights-engagement-dashboard.overdueAssignments": "Users currently have one or more overdue assignments.",
+
+	"components.insights-applied-filters.clear-all": "Clear all"
 };
