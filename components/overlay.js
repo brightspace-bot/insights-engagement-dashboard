f8a1f7f5ec58781d112ffbe3a11b1d935c618d9a
Vitalii Misechko <Vitalii.Misechko@d2l.com>
2020-09-15 14:58:07 +0300

    Add Overlay component

---
+++ components/overlay.js
@@ -0,0 +1,59 @@
+import '@brightspace-ui/core/components/loading-spinner/loading-spinner.js';
+
+import { css, html, LitElement } from 'lit-element/lit-element.js';
+
+/**
+ * Requires that parent element has non-static position property. For instance, position: relative;
+ *
+ * @property {Number} spinner-size
+ * @property {Boolean} loading
+ */
+class Overlay extends LitElement {
+	static get properties() {
+		return {
+			spinnerSize: { type: Number, attribute: 'spinner-size' },
+			isLoading: { type: Boolean, attribute: 'loading' }
+		};
+	}
+
+	static get styles() {
+		return css`
+			:host {
+				display: block;
+				position: absolute;
+				top: 0;
+				left: 0;
+				height: 100%;
+				width: 100%;
+				z-index: 1;
+			}
+			:host([hidden]) {
+				display: none;
+			}
+
+			.d2l-insights-overlay {
+				background-color: white;
+				height: 100%;
+				width: 100%;
+				border-radius: 15px;
+				margin: 1 1 1 1;
+				display: flex;
+				align-items: center;
+    			justify-content: center;
+			}
+		`;
+	}
+
+	render() {
+		if (!this.isLoading) {
+			return html``;
+		}
+
+		return html`
+			<div class="d2l-insights-overlay">
+				<d2l-loading-spinner size="${this.spinnerSize}"></d2l-loading-spinner>
+			</div>`;
+	}
+
+}
+customElements.define('d2l-insights-overlay', Overlay);
