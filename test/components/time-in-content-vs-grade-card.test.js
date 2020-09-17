a22c44ab5993748e1f51561284cc5c97edc61bb8
Vitalii Misechko <Vitalii.Misechko@d2l.com>
2020-09-17 15:28:57 +0300

    Add unit tests

--- test/components/time-in-content-vs-grade-card.test.js
+++ test/components/time-in-content-vs-grade-card.test.js
@@ -1,6 +1,6 @@
 import '../../components/time-in-content-vs-grade-card';
 
-import { expect, fixture, html } from '@open-wc/testing';
+import { elementUpdated, expect, fixture, html } from '@open-wc/testing';
 import { runConstructor } from '@brightspace-ui/core/tools/constructor-test-helper.js';
 
 describe('d2l-insights-time-in-content-vs-grade-card', () => {
@@ -27,6 +27,20 @@ describe('d2l-insights-time-in-content-vs-grade-card', () => {
 			const title = (el.shadowRoot.querySelectorAll('div.d2l-insights-time-in-content-vs-grade-title'));
 			expect(title[0].innerText).to.equal('Time in Content vs. Grade');
 			expect(el._preparedPlotData).to.equal(data.currentFinalGradesVsTimeInContent);
+		});
+
+		it('should exclude chart form tabindex when data is loading', async() => {
+			const loadingData = Object.assign({}, data, { isLoading: true });
+			const el = await fixture(html`<d2l-insights-time-in-content-vs-grade-card .data="${loadingData}"></d2l-insights-time-in-content-vs-grade-card>`);
+
+			const chart = el.shadowRoot.querySelector('d2l-labs-chart');
+			const chartDiv = chart.shadowRoot.querySelector('#chart-container');
+			expect(chartDiv.getAttribute('tabindex')).to.equal('-1');
+
+			loadingData.isLoading = false;
+			el.requestUpdate();
+			await elementUpdated(el);
+			expect(chartDiv.getAttribute('tabindex')).to.equal('0');
 		});
 	});
 });
