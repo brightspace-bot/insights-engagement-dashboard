5cbf24dc5ba5171f46f241a304a3103722797c5d
MykolaGal <gnnkolya@gmail.com>
2020-10-08 11:47:45 +0300

    Update engagement-dashboard.js

--- engagement-dashboard.js
+++ engagement-dashboard.js
@@ -58,7 +58,8 @@ class EngagementDashboard extends Localizer(MobxLitElement) {
 				}
 
 				.d2l-insights-summary-container-applied-filters {
-					height: 80px;
+					min-height: 30px;
+  					height: auto;
 					width: 100%;
 				}
 
