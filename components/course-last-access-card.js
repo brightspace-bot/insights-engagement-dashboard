183970622c9b54bce60bca144850bc2ba8124f21
Nick Hallman <Nick.Hallman@D2L.com>
2020-10-09 15:10:13 -0400

    undo space

--- components/course-last-access-card.js
+++ components/course-last-access-card.js
@@ -157,7 +157,7 @@ class CourseLastAccessCard extends SkeletonMixin(Localizer(MobxLitElement)) {
 	_pointUpdateColor(point, colorForPoint) {
 		point.update({ color: colorForPoint }, false);
 	}
- 
+
 	render() {
 		// NB: relying on mobx rather than lit-element properties to handle update detection: it will trigger a redraw for
 		// any change to a relevant observed property of the Data object
