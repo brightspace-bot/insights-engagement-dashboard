f980bf9a4a1abc5fa637453222976614c4cab8ca
Vitalii Misechko <Vitalii.Misechko@d2l.com>
2020-11-02 14:46:27 +0200

    Revert "Karama souce conf"
    This reverts commit bba73b819a9fbc2e1a072a8504ca2575cd380b54.

--- karma.sauce.conf.js
+++ karma.sauce.conf.js
@@ -28,12 +28,6 @@ const customLaunchers = {
 module.exports = config => {
 	config.set(
 		merge(createDefaultConfig(config), {
-			// from https://support.saucelabs.com/hc/en-us/articles/225104707-Karma-Tests-Disconnect-Particularly-When-Running-Tests-on-Safari
-			// to avoid DISCONNECTED messages
-			browserDisconnectTimeout: 10000, // default 2000
-			browserDisconnectTolerance: 1, // default 0
-			browserNoActivityTimeout: 4 * 60 * 1000, //default 10000
-			captureTimeout: 4 * 60 * 1000, //default 60000
 			files: [
 				// runs all files ending with .test in the test folder,
 				// can be overwritten by passing a --grep flag. examples:
@@ -53,12 +47,7 @@ module.exports = config => {
 			customLaunchers: customLaunchers,
 			browsers: Object.keys(customLaunchers),
 			reporters: ['dots', 'saucelabs'],
-			singleRun: true,
-			client: {
-				mocha: {
-					timeout: 20000 // 20 seconds
-				}
-			}
+			singleRun: true
 		}),
 	);
 	return config;
