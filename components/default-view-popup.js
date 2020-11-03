cd83f04e668d315474e39ceef18ea331981bff17
Vitalii Misechko <Vitalii.Misechko@d2l.com>
2020-11-03 18:13:07 +0200

    rename private function

--- components/default-view-popup.js
+++ components/default-view-popup.js
@@ -58,7 +58,7 @@ class DefaultViewPopup extends RtlMixin(Localizer(LitElement)) {
 					width="615"
 					@d2l-dialog-close="${this._closeDialog}">
 
-				${this._renderDescription1({ displayData, numDefaultCourses: displayData.length, numDefaultSemesters: this._defaultSemestersCount })}
+				${this._renderDescriptiveEmptyResultsMessage({ displayData, numDefaultCourses: displayData.length, numDefaultSemesters: this._defaultSemestersCount })}
 
 				<p class="d2l-insights-default-view-popup-description">
 					${this.localize('components.insights-default-view-popup.promptUseFilters')}
@@ -73,7 +73,7 @@ class DefaultViewPopup extends RtlMixin(Localizer(LitElement)) {
 		`;
 	}
 
-	_renderDescription1({ displayData, numDefaultCourses, numDefaultSemesters }) {
+	_renderDescriptiveEmptyResultsMessage({ displayData, numDefaultCourses, numDefaultSemesters }) {
 		if (displayData.length) {
 			return html`
 				<p class="d2l-insights-default-view-popup-description">
